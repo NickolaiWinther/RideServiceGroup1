80b2b0fc6e5e3719d837c38c60b923fa882671c4
Nickolai Schioldan Winther <nick3846@edu.campusvejle.dk>
2019-04-11 11:35:54 +0200

    Added data to the tables

--- Create and delete tables SQL.sql
+++ Create and delete tables SQL.sql
@@ -6,11 +6,11 @@ DROP TABLE Reports
 IF EXISTS(select * from INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Rides')
 DROP TABLE Rides
 
-IF EXISTS(select * from INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'RideCategory')
-DROP TABLE RideCategory
+IF EXISTS(select * from INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'RideCategories')
+DROP TABLE RideCategories
 
 
-CREATE TABLE RideCategory (
+CREATE TABLE RideCategories (
 	RideCategoryId INT PRIMARY KEY IDENTITY(1,1),
 	Name NVARCHAR(50) NOT NULL,
 	Description NVARCHAR(MAX) NOT NULL
@@ -20,7 +20,7 @@ CREATE TABLE Rides (
 	RideId INT PRIMARY KEY IDENTITY(1,1),
 	Name NVARCHAR(50) NOT NULL,
 	Description NVARCHAR(MAX) NOT NULL,
-	CategoryId INT NOT NULL FOREIGN KEY REFERENCES RideCategory(RideCategoryId),
+	CategoryId INT NOT NULL FOREIGN KEY REFERENCES RideCategories(RideCategoryId),
 	Status INT NOT NULL
 );
 
@@ -33,4 +33,37 @@ CREATE TABLE Reports (
 );
 
 
+-- RideCategory
+INSERT INTO RideCategories
+VALUES ('Vandforlystelse', 'Baner der har med vand at gøre')
 
+INSERT INTO RideCategories
+VALUES ('Pendulforlystelse', 'Baner der har med penduler at gøre')
+
+INSERT INTO RideCategories
+VALUES ('Rutsjebane', 'En rutsjebane')
+
+
+-- Rides
+INSERT INTO Rides
+VALUES ('Vandfaldet', '42 meter lang vandrutsjebane, der kun går en vej: Ned!', 1, 3)
+
+INSERT INTO Rides
+VALUES ('Regnskoven', 'Stort vandlegeland med masser of forhindringer og rutsjebaner', 1, 1)
+
+INSERT INTO Rides
+VALUES ('Hvirvelvinden', 'En forlystelse for de største vovehalse', 2, 1)
+
+INSERT INTO Rides
+VALUES('Nøddesvinget', 'Nyhed 2019! Kører både forlæns og baglæns', 2, 2)
+
+INSERT INTO Rides
+VALUES ('Falken', 'Danmarks hurtigste trærutsjebane', 3, 2)
+
+INSERT INTO Rides
+VALUES ('Orkanen', 'Orkanen er en fantastisk og hæsblæsende rutsjebane for hele familien', 3, 3)
+
+
+-- Reports
+INSERT INTO Reports
+VALUES (''
