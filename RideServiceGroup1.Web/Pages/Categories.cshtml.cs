7733c334b706a201e423c3f8797eda749c7e5afb
Nickolai Schioldan Winther <nick3846@edu.campusvejle.dk>
2019-04-30 10:32:26 +0200

    You can now create a category

--- RideServiceGroup1.Web/Pages/Categories.cshtml.cs
+++ RideServiceGroup1.Web/Pages/Categories.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,13 +12,29 @@ namespace RideServiceGroup1.Web.Pages
 {
     public class CategoriesModel : PageModel
     {
+        [BindProperty]
+        [Display(Name = "Navn")]
         public string CategoryName { get; set; }
+        [BindProperty]
+        [Display(Name = "Beskrivelse")]
         public string CategoryDescription { get; set; }
-        public List<RideCategory> Categories { get; set; }
+        public List<RideCategory> Categories { get; set; } = new List<RideCategory>();
         public void OnGet()
         {
             CategoryRepository categoryRepository = new CategoryRepository();
             Categories = categoryRepository.GetAll();
+        }
+        public void OnPost()
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            RideCategory rideCategory = new RideCategory()
+            {
+                Name = CategoryName,
+                Description = CategoryDescription
+            };
+            categoryRepository.Insert(rideCategory);
+            Categories = categoryRepository.GetAll();
+
         }
     }
 }
